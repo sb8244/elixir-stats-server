0454d734f80a8ee876563078d5f89c24ac4c44ab
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-06 23:52:48 -0400

    an encrypted command is pushed from the server socket when the pubsub topic is received

--- lib/stats_server_web/channels/server_channel.ex
+++ lib/stats_server_web/channels/server_channel.ex
@@ -1,7 +1,15 @@
 defmodule StatsServerWeb.ServerChannel do
   use Phoenix.Channel
 
+  intercept(["dispatch_command"])
+
   def join("server", _message, socket) do
     {:ok, socket}
+  end
+
+  def handle_out("dispatch_command", payload = %{command_id: _, encrypted_command: _}, socket) do
+    push socket, "dispatch_command", payload
+
+    {:noreply, socket}
   end
 end
--- test/stats_server_web/channels/server_channel_test.exs
+++ test/stats_server_web/channels/server_channel_test.exs
@@ -16,4 +16,12 @@ defmodule StatsServerWeb.ServerChannelTest do
       start_socket()
     end
   end
+
+  describe "handle_out dispatch_command" do
+    test "the socket is pushed the command" do
+      socket = start_socket()
+      broadcast_from!(socket, "dispatch_command", %{command_id: "a", encrypted_command: "b"})
+      assert_push "dispatch_command", %{command_id: "a", encrypted_command: "b"}
+    end
+  end
 end
