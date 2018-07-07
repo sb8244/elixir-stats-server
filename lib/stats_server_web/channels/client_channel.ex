0f9317e0b726232259a6ab0dc8716074f4ee9735
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-06 22:55:49 -0400

    list application_names from the client socket

--- lib/stats_server_web/channels/client_channel.ex
+++ lib/stats_server_web/channels/client_channel.ex
@@ -1,7 +1,13 @@
 defmodule StatsServerWeb.ClientChannel do
   use Phoenix.Channel
 
+  alias StatsServer.Config
+
   def join("client", _message, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("application_names", _payload, socket) do
+    {:reply, {:ok, %{application_names: Config.application_names()}}, socket}
   end
 end
--- test/stats_server_web/channels/client_channel_test.exs
+++ test/stats_server_web/channels/client_channel_test.exs
@@ -10,4 +10,15 @@ defmodule StatsServerWeb.ClientChannelTest do
         |> subscribe_and_join(ClientChannel, "client", %{})
     end
   end
+
+  describe "handle_in application_names" do
+    test "the application names are returned" do
+      {:ok, _, socket} =
+        socket("client_socket", %{})
+        |> subscribe_and_join(ClientChannel, "client", %{})
+
+      ref = push(socket, "application_names", %{})
+      assert_reply(ref, :ok, %{application_names: ["Test", "Other Test"]})
+    end
+  end
 end
