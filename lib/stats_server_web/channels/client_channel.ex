4b4467efe509688afa6048056402453b55044a06
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-06 23:31:43 -0400

    dispatch a command from the frontend to the server topic

--- lib/stats_server_web/channels/client_channel.ex
+++ lib/stats_server_web/channels/client_channel.ex
@@ -10,4 +10,14 @@ defmodule StatsServerWeb.ClientChannel do
   def handle_in("application_names", _payload, socket) do
     {:reply, {:ok, %{application_names: Config.application_names()}}, socket}
   end
+
+  def handle_in("dispatch_command", %{"application_name" => app_name, "command_id" => id, "encrypted_command" => command}, socket) do
+    server_params = %{
+      command_id: id,
+      encrypted_command: command
+    }
+    StatsServerWeb.Endpoint.broadcast("server:#{app_name}", "dispatch_command", server_params)
+
+    {:reply, {:ok, %{}}, socket}
+  end
 end
--- test/stats_server_web/channels/client_channel_test.exs
+++ test/stats_server_web/channels/client_channel_test.exs
@@ -3,22 +3,36 @@ defmodule StatsServerWeb.ClientChannelTest do
 
   alias StatsServerWeb.ClientChannel
 
+  def start_socket() do
+    {:ok, _, s = %Phoenix.Socket{}} =
+      socket("client_socket", %{})
+      |> subscribe_and_join(ClientChannel, "client", %{})
+
+    s
+  end
+
   describe "join" do
     test "the socket can be joined" do
-      {:ok, _, %Phoenix.Socket{}} =
-        socket("client_socket", %{})
-        |> subscribe_and_join(ClientChannel, "client", %{})
+      start_socket()
     end
   end
 
   describe "handle_in application_names" do
     test "the application names are returned" do
-      {:ok, _, socket} =
-        socket("client_socket", %{})
-        |> subscribe_and_join(ClientChannel, "client", %{})
-
-      ref = push(socket, "application_names", %{})
+      ref = push(start_socket(), "application_names", %{})
       assert_reply(ref, :ok, %{application_names: ["Test", "Other Test"]})
+    end
+  end
+
+  describe "handle_in dispatch_command" do
+    test "the server:application_name topic is broadcasted the encrypted command" do
+      StatsServerWeb.Endpoint.subscribe("server:Test")
+
+      ref = push(start_socket(), "dispatch_command", %{application_name: "Test", encrypted_command: "x", command_id: "a"})
+      assert_reply(ref, :ok, %{})
+
+      expected_payload = %{command_id: "a", encrypted_command: "x"}
+      assert_receive %Phoenix.Socket.Broadcast{event: "dispatch_command", payload: ^expected_payload, topic: "server:Test"}
     end
   end
 end
