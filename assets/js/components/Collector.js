ceed7965dd011a71d3c9d6ba193358d9546c16ee
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-11 23:11:21 -0400

    add in a dead simple / not great visualization of stats

--- assets/js/components/Collector.js
+++ assets/js/components/Collector.js
@@ -8,16 +8,50 @@ function groupStatsByCommandId(eventList) {
   return groupBy(statsEvents, "commandId")
 }
 
+function appGrouping(appGroup) {
+  const commandGrouped = groupStatsByCommandId(appGroup)
+
+  return (
+    <div>
+      {
+        Object.keys(commandGrouped).map((commandId) => (
+          <div key={commandId}>
+            <h3>{commandId}</h3>
+            <div className="server-stats-list">
+              {serverStatsGroup(commandGrouped[commandId])}
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+function serverStatsGroup(statsArr) {
+  return statsArr.map(({ collectedAt, commandId, serverId, stats }) => (
+    <div className="server-stats-list__entry" key={`${commandId}-${serverId}`}>
+      <div>{serverId}</div>
+      {
+        stats.map(({ label, value }) => (
+          <div key={label}>
+            <strong>{label}:&nbsp;</strong>
+            <span>{value}</span>
+          </div>
+        ))
+      }
+    </div>
+  ))
+}
+
 export default () => (
   <CollectorStateContext.Consumer>
   {
     (collected) => (
       Object.keys(collected).map((appName) => (
         <div key={appName}>
-          <h3>{appName}</h3>
-          <div>
-            {JSON.stringify(groupStatsByCommandId(collected[appName]))}
-          </div>
+          <h2>{appName}</h2>
+
+          {appGrouping(collected[appName])}
         </div>
       ))
     )
