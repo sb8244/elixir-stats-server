6936313410bf9654b36d9fa987b30b52deba56ca
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-19 01:06:54 -0400

    show the color of the server in the server list

--- assets/js/components/ServerList.js
+++ assets/js/components/ServerList.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react'
 import sortBy from 'lodash/sortBy'
 
 import { ServerListStateContext } from './ServerListState'
+import { ServerColorStateContext } from './ServerColorState'
 
 export default () => (
-  <ServerListStateContext.Consumer>
+  <ServerColorStateContext.Consumer>
   {
-    (servers) => (
-      <div className="server-list-wrapper">
-        <h1>Servers ({servers.length})</h1>
-        <ul>
-          {
-            sortBy(servers, 'server_id').map(({ server_id }) => (
-              <li key={server_id}>{server_id}</li>
-            ))
-          }
-        </ul>
-      </div>
+    ({ getColor }) => (
+      <ServerListStateContext.Consumer>
+      {
+        (servers) => (
+          <div className="server-list-wrapper">
+            <h1>Servers ({servers.length})</h1>
+            <ul>
+              {
+                sortBy(servers, 'server_id').map(({ server_id }) => (
+                  <li key={server_id}>
+                    <span className="color-legend-block" style={{background: getColor(server_id)}} />
+                    <span>{server_id}</span>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        )
+      }
+      </ServerListStateContext.Consumer>
     )
   }
-  </ServerListStateContext.Consumer>
+  </ServerColorStateContext.Consumer>
 )
