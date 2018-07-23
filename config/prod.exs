152aefa98cf7f3193f38f6b01057591fd207f31a
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-23 00:43:29 -0400

    hanging chad

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :stats_server, StatsServerWeb.Endpoint,
   load_from_system_env: true,
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :phoenix, :serve_endpoints, true
 
