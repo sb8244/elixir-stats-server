36bd55768cd345e5a505d282b70db86af73ab772
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-22 20:51:35 -0400

    select which collector view to display

--- assets/js/components/Collector.js
+++ assets/js/components/Collector.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { CollectorStateContext } from './CollectorState'
 import { ServerListStateContext } from './ServerListState'
@@ -6,21 +6,43 @@ import { ServerListStateContext } from './ServerListState'
 import ChartContainer from './Collector/ChartContainer'
 import TextsContainer from './Collector/TextsContainer'
 
-export default () => (
-  <div className="charts-wrapper">
-    <ServerListStateContext.Consumer>
-    {
-      ({ getColor }) => (
-        <CollectorStateContext.Consumer>
-        {
-          ({ chartData, plainTextLogs }) => ([
-            <ChartContainer key='chart-container' getColor={getColor} chartData={chartData} />,
-            <TextsContainer key='texts-container' getColor={getColor} plainTextLogs={plainTextLogs} />,
-          ])
-        }
-        </CollectorStateContext.Consumer>
-      )
-    }
-    </ServerListStateContext.Consumer>
-  </div>
-)
+const CHART_TAB = 'charts'
+const TEXTS_TAB = 'texts'
+
+export default class Collector extends Component {
+  state = {
+    selectedTab: CHART_TAB
+  }
+
+  render() {
+    const { selectedTab } = this.state
+
+    return (
+      <div>
+        <div className="collector-header">
+          <button onClick={() => this.setState({ selectedTab: CHART_TAB })}>Charts</button>
+          <button onClick={() => this.setState({ selectedTab: TEXTS_TAB })}>Text Outputs</button>
+        </div>
+        <div className="collector-view-wrap">
+          <ServerListStateContext.Consumer>
+          {
+            ({ getColor }) => (
+              <CollectorStateContext.Consumer>
+              {
+                ({ chartData, plainTextLogs }) => {
+                  if (selectedTab === CHART_TAB) {
+                    return <ChartContainer getColor={getColor} chartData={chartData} />
+                  } else if (selectedTab === TEXTS_TAB) {
+                    return <TextsContainer getColor={getColor} plainTextLogs={plainTextLogs} />
+                  }
+                }
+              }
+              </CollectorStateContext.Consumer>
+            )
+          }
+          </ServerListStateContext.Consumer>
+        </div>
+      </div>
+    )
+  }
+}
