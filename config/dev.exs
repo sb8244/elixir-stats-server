8b7223e3e83d02051d77a545cdb91ecd825c9a26
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-23 00:41:55 -0400

    we don't need to set the url for this simple of an app

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,6 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :stats_server, StatsServerWeb.Endpoint,
   http: [port: 4000],
-  url: [host: "localhost"],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
