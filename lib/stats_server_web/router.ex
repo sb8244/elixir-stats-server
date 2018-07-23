07cf9917ab2705bda8ed69d62cd4725df163e04c
Stephen Bussey <sb8244@cs.ship.edu>
2018-07-23 00:41:06 -0400

    remove things relying on a session

--- lib/stats_server_web/router.ex
+++ lib/stats_server_web/router.ex
@@ -3,9 +3,6 @@ defmodule StatsServerWeb.Router do
 
   pipeline :browser do
     plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
   end
 
